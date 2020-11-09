c6e37ef0184515c49973706a4a2424dcfad67e47
kniffenfork <fomin.as@phystech.edu>
2020-11-09 20:09:23 +0300

    Initial

--- rush01/ex00/head_sudoku.h
+++
@@ -1,7 +0,0 @@
-#ifndef LEARN_C_HEAD_SUDOKU_H
-#define LEARN_C_HEAD_SUDOKU_H
-
-#include <stdlib.h>
-#include "../../day06/ex00/include/libft.h"
-
-#endif //LEARN_C_HEAD_SUDOKU_H
--- rush01/ex00/solve_sudoku.c
+++
@@ -1,3 +0,0 @@
-#include "head_sudoku.h"
-
-char solution_of_sudoku()
