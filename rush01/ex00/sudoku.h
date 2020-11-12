2b142c571024f22fe67b10004428fcb09451bdbd
kniffenfork <fomin.as@phystech.edu>
2020-11-12 18:20:28 +0300

    This fucking shit works

--- rush01/ex00/sudoku.h
+++ rush01/ex00/sudoku.h
@@ -18,29 +18,23 @@ typedef struct sudoku
 
 }              t_sudoku;
 
-typedef struct coord
-{
-    int        line;
-    int        column;
-}              coordinates;
-
 
 // operations with sudoku structure
-t_sudoku *fill_sudoku_structure(int ac, char **av);
-void show_sudoku(t_sudoku *par);
+t_sudoku    *fill_sudoku_structure(int ac, char **av);
+void        show_sudoku(t_sudoku *par);
 
 
 // check for uniqueness
 t_bool      one_in_line(t_sudoku *sudoku, char symbol_to_try, int line);
 t_bool      one_in_column(t_sudoku *sudoku, char symbol_to_try, int column);
 t_bool      one_in_square(t_sudoku *sudoku, char symbol_to_try, int line, int column);
-int *go_to_start_of_square(int line, int column);
+int         *go_to_start_of_square(int line, int column);
 t_bool      is_unique(t_sudoku *sudoku, char symbol_to_try, int line, int column);
 
 
 // solve this fucking sudoku
-t_bool check_sudoku(t_sudoku *sudoku);
-int solve(t_sudoku *sudoku);
+t_bool      check_sudoku(t_sudoku *sudoku);
+int         solve(t_sudoku *sudoku);
 
 
 #endif //LEARN_C_SUDOKU_H
